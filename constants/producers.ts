0caad4410fca3101c18122d23063a53b3e9fcddd
iambnlvn <iambnlvn@gmai.com>
2023-08-16 16:33:01 -0500

    Changed path of producers videos after compression

--- constants/producers.ts
+++ constants/producers.ts
@@ -8,25 +8,25 @@ export const producers = [{
   id: 0,
   image: disney,
   videoUrl:
-    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1690982846/disney_rijvne.mp4",
+    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1692219327/disney_ff058v.webm",
 }, {
   id: 1,
   image: pixar,
   videoUrl:
-    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1690982851/pixar_qmxooa.mp4",
+    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1692219327/pixar_xpsrrf.webm",
 }, {
   id: 2,
   image: marvel,
   videoUrl:
-    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1690982844/marvel_btpttw.mp4",
+    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1692219331/marvel_q0m6g4.webm",
 }, {
   id: 3,
   image: natGeo,
   videoUrl:
-    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1690982852/natGeo_iqfbub.mp4",
+    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1692219327/natGeo_mmd4cs.webm",
 }, {
   id: 4,
   image: starWars,
   videoUrl:
-    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1690982844/starWars_ubcgns.mp4",
+    "https://res.cloudinary.com/dk3zkgj4n/video/upload/v1692219331/starWars_n2pz0d.webm",
 }] as const;
